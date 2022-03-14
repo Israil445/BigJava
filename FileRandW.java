e44b92ff567c34e13fa7bc0397ff2b16f3dd4f89
Israil445 <97037120+Israil445@users.noreply.github.com>
2022-03-14 14:48:17 +0600

    Add files via upload

---
+++ FileRandW.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class FileRandW {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file=new File("/home/israil/IdeaProjects/lab/output.txt");
+        //now write a file
+        try(PrintWriter out= new PrintWriter(file)){
+           out.println("israil");
+           out.println("CSE");
+        }
+
+        //read a file
+
+        try(Scanner in= new Scanner(file)){
+            while(in.hasNext()){
+                String line=in.nextLine();
+                System.out.println(line);
+            }
+
+        }
+
+
+
+
+
+
+
+    }
+}
